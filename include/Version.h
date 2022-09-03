1eb3aa9b2be0323594b0f734abf2f36e002ca117
Jeff <junk@jeffhighsmith.com>
2022-09-03 14:11:33 -0400

    Feat: buttons reading through MCP23017. LED not working yet.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.5"
+        #define VERSION "0.1.8"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-09-02 22:06:40.931163"
+        #define BUILD_TIMESTAMP "2022-09-03 13:53:40.983179"
     #endif
     
--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,53 @@
 #include "Version.h"
 #include <pinout.h>
 #include <audio.h>
-#include <radio.h>
+// #include <radio.h>
+#include <Adafruit_MCP23X17.h>
+
+
+#include <Adafruit_DotStar.h>
+#include <SPI.h> // COMMENT OUT THIS LINE FOR GEMMA OR TRINKET
+
+#define NUMPIXELS 16 // Number of LEDs in strip
+
+// Here's how to control the LEDs from any two pins:
+#define DATAPIN A4
+#define CLOCKPIN A5
+Adafruit_DotStar strip(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
+// The last parameter is optional -- this is the color data order of the
+// DotStar strip, which has changed over time in different production runs.
+// Your code just uses R,G,B colors, the library then reassigns as needed.
+// Default is DOTSTAR_BRG, so change this if you have an earlier strip.
+
+
 
 // Button Header
 // Include the Bounce2 library found here :
 // https://github.com/thomasfredericks/Bounce2
-#include <Bounce2.h>
+// #include <Bounce2.h>
+
+#define DEBOUNCE_DELAY 50
+
+struct Pushbutton {
+  long last_time_changed;
+  bool last_state;
+  int led_one;
+  int led_two;
+};
 
 // INSTANTIATE A Bounce OBJECT
-Bounce bounce = Bounce();
+// b = Bounce()
 
-// SET A VARIABLE TO STORE THE LED STATE
-int ledState = LOW;
+Pushbutton buttons[8];
+
+
+Adafruit_MCP23X17 mcp;
+
+
+int head = 0, tail = -10;  // Index of first 'on' and 'off' pixels
+uint32_t color = 0xFF0000; // 'On' color (starts red)
+
+
 
 // ███████╗███████╗████████╗██╗   ██╗██████╗
 // ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
@@ -34,24 +69,45 @@ void setup()
   }
   Serial.printf("\nProject version v%s, built %s\n", VERSION, BUILD_TIMESTAMP);
   Serial.println("Setup function commencing...");
-  vsAudioSetup();
+  // vsAudioSetup();
   delay(100);
-  radioSetup();
+  // radioSetup();
+
+
+  if (!mcp.begin_I2C())
+  {
+      Serial.println("Error initializing MCP23017.");
+      //while (1)
+      //    ;
+  }
+
+  // configure button pin for input with pull up
+  // mcp.pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  for(int i=0; i<8; i++) {
+    mcp.pinMode(i, INPUT_PULLUP);
+    buttons[i].last_state = mcp.digitalRead(i);
+    buttons[i].last_time_changed = millis();
+  }
+
+
 
   // BOUNCE SETUP
 
   // SELECT ONE OF THE FOLLOWING :
   // 1) IF YOUR INPUT HAS AN INTERNAL PULL-UP
-  bounce.attach(BOUNCE_PIN, INPUT_PULLUP); // USE INTERNAL PULL-UP
+  // bounce.attach(BOUNCE_PIN, INPUT_PULLUP); // USE INTERNAL PULL-UP
   // 2) IF YOUR INPUT USES AN EXTERNAL PULL-UP
   // bounce.attach( BOUNCE_PIN, INPUT ); // USE EXTERNAL PULL-UP
 
   // DEBOUNCE INTERVAL IN MILLISECONDS
-  bounce.interval(5); // interval in ms
+  // bounce.interval(5); // interval in ms
 
-  // LED SETUP
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, ledState);
+
+  strip.begin(); // Initialize pins for output
+  strip.show();  // Turn all LEDs off ASAP
+
+
 
   Watchdog.enable(4000);
   Serial.println("Setup Complete");
@@ -67,25 +123,42 @@ void setup()
 void loop()
 {
   // Update the Bounce instance (YOU MUST DO THIS EVERY LOOP)
-  bounce.update();
+  // bounce.update();
 
-  // <Bounce>.changed() RETURNS true IF THE STATE CHANGED (FROM HIGH TO LOW OR LOW TO HIGH)
-  if (bounce.changed())
-  {
-    // THE STATE OF THE INPUT CHANGED
-    // GET THE STATE
-    int deboucedInput = bounce.read();
-    // IF THE CHANGED VALUE IS LOW
-    if (deboucedInput == LOW)
-    {
-      ledState = !ledState;            // SET ledState TO THE OPPOSITE OF ledState
-      digitalWrite(LED_PIN, ledState); // WRITE THE NEW ledState
-      startAudio();
-      delay(1000);
-      stopAudio();
-      sendGoEvent(1); // Does not work inside VS1053 audio startPlayingFile!
+  for(int i=0; i<8; i++) {
+    int state = mcp.digitalRead(i);
+    if ((millis() - buttons[i].last_time_changed) > DEBOUNCE_DELAY) {
+      if (state != buttons[i].last_state) {
+        if (state == LOW) {
+          // Play sound
+          //startAudio(); // TODO: pass sound number //
+
+          // If LED array looks right, add self, otherwise clear and add self
+          Serial.printf("Button %d pressed.\n", i);
+        } else {
+          Serial.printf("Button %d released.\n", i);
+        }
+        buttons[i].last_state = state;
+        buttons[i].last_time_changed = millis();
+      }
     }
   }
+
+
+  strip.setPixelColor(head, color); // 'On' pixel at head
+  strip.setPixelColor(tail, 0);     // 'Off' pixel at tail
+  strip.show();                     // Refresh strip
+  delay(20);                        // Pause 20 milliseconds (~50 FPS)
+
+  if (++head >= NUMPIXELS)
+  {                           // Increment head index.  Off end of strip?
+      head = 0;               //  Yes, reset head index to start
+      if ((color >>= 8) == 0) //  Next color (R->G->B) ... past blue now?
+          color = 0xFF0000;   //   Yes, reset to red
+  }
+  if (++tail >= NUMPIXELS)
+      tail = 0; // Increment, reset tail index
+
 
   Watchdog.reset();
 }
