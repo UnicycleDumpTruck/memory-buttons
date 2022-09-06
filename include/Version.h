6dc362f9edcb8823d0c39885f147ecf1f3e6fa75
Jeff <junk@jeffhighsmith.com>
2022-09-06 14:59:18 -0400

    Feat:Memory game working with hardware.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.9"
+        #define VERSION "0.1.23"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-09-03 14:37:58.145370"
+        #define BUILD_TIMESTAMP "2022-09-06 14:57:43.715184"
     #endif
     
--- src/main.cpp
+++ src/main.cpp
@@ -49,8 +49,113 @@ Adafruit_MCP23X17 mcp;
 
 
 int head = 0, tail = -10;  // Index of first 'on' and 'off' pixels
-uint32_t color = 0xFF0000; // 'On' color (starts red)
+//uint32_t color = 0xFF0000; // 'On' color (starts red)
 
+
+
+
+// Color for each of the 16 LEDs in the strip. Starts as off.
+uint64_t led_strip[] = {0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000};
+
+// Next position in the ORDER of buttons that could be filled. 0 when empty, 7 when full.
+int next_lit_position = 0;
+
+// Whether the order is correct so far, since the last clearing.
+bool order_correct = true;
+
+
+/* 
+* The following arrays store data for each of the eight buttons.
+* All of button zero's data is in index zero of each of these.
+*/
+
+// Color for each button:
+uint64_t color[] = {0x00FF00, 0x00FF00, 0x888800, 0x888800, 0xFF0000, 0xFF0000, 0x0000FF, 0x0000FF};
+
+// Correct place in line each button should be pressed:
+int correct_position[] = {6, 2, 4, 7, 1, 5, 3, 0}; // TODO: randomize this after success
+
+// Last electrical state button was read at, true == HIGH == not-pressed
+bool last_state[] = {true, true, true, true, true, true, true, true};
+
+// Last time (monotonic) each button was changed, used for debouncing:
+long last_time_changed[8];
+
+// Whether leds for each button are lit:
+bool lit[] = {false, false, false, false, false, false, false, false};
+
+// Variable to store input from std in:
+int input = -1;
+
+
+void clearStrip(){
+  for (int i=0; i<8; i++) {
+      lit[i] = false;
+      int pos_1 = correct_position[i]*2;
+      int pos_2 = (correct_position[i]*2) + 1;
+      led_strip[pos_1] = 0x000000;
+      led_strip[pos_2] = 0x000000;
+      strip.setPixelColor(pos_1, 0x000000);
+      strip.setPixelColor(pos_2, 0x000000);
+      Serial.printf("Cleared positions %d and %d\n", pos_1, pos_2);
+      next_lit_position = 0;
+  }
+}
+
+void processButton(int input) {
+  if (lit[input] == false) { // if this button not lit
+    Serial.printf("nlp: %d, cor_pos: %d", next_lit_position, correct_position[input]);
+
+    if (next_lit_position > 7) { // if not yet solved
+      clearStrip();
+    } else {
+      if (next_lit_position != correct_position[input]) { // if wrong button
+        Serial.println("Wrong Order.");
+        order_correct = false;
+      }
+      if (order_correct == false) {
+        Serial.println("Clearing the led strip.");
+        clearStrip();
+      }
+
+      // Light the button's leds
+      lit[input] = true; // Record button as lit.
+      int pos_1 = correct_position[input]*2;
+      int pos_2 = (correct_position[input]*2) + 1;
+      led_strip[pos_1] = color[input];
+      led_strip[pos_2] = color[input];
+      strip.setPixelColor(pos_1, color[input]);
+      strip.setPixelColor(pos_2, color[input]);
+      Serial.printf("Added color to pixels %d and %d\n", pos_1, pos_2);
+
+      next_lit_position++; // Advance the order position tracker.
+
+      // If that was the right button, record order as correct so far.
+      if ((correct_position[input] == 0) && (next_lit_position == 1)) {
+          order_correct = true;
+      }
+    }
+
+  } else {
+    if (next_lit_position > 7) { // if not yet solved
+      Serial.println("Puzzle was solved, now clearing strip.");
+      clearStrip();
+    }
+
+      // Button was already lit
+    Serial.println("button already lit");
+      // TODO: Possibly unlight the button, but would that make it too easy,
+      // as one could just light and unlight buttons until they found the
+      // next one to light in order?
+  }
+  
+  // Debug statement, print LED strip color contents.
+  for (int i=0; i<16; i++) {
+      Serial.printf("%d", led_strip[i]);
+  }
+  Serial.println(" ");
+  strip.show();
+} // End processButton()
 
 
 // ███████╗███████╗████████╗██╗   ██╗██████╗
@@ -135,6 +240,7 @@ void loop()
 
           // If LED array looks right, add self, otherwise clear and add self
           Serial.printf("Button %d pressed.\n", i);
+          processButton(i);
         } else {
           Serial.printf("Button %d released.\n", i);
         }
@@ -143,22 +249,6 @@ void loop()
       }
     }
   }
-
-
-  strip.setPixelColor(head, color); // 'On' pixel at head
-  strip.setPixelColor(tail, 0);     // 'Off' pixel at tail
-  strip.show();                     // Refresh strip
-  delay(20);                        // Pause 20 milliseconds (~50 FPS)
-
-  if (++head >= NUMPIXELS)
-  {                           // Increment head index.  Off end of strip?
-      head = 0;               //  Yes, reset head index to start
-      if ((color >>= 8) == 0) //  Next color (R->G->B) ... past blue now?
-          color = 0xFF0000;   //   Yes, reset to red
-  }
-  if (++tail >= NUMPIXELS)
-      tail = 0; // Increment, reset tail index
-
 
   Watchdog.reset();
 }
